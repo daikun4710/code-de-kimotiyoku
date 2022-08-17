2ecafc2da021a8e462306ed6362e1376676246f1
yuksaki72 <109458803+yuksaki72@users.noreply.github.com>
2022-08-18 04:33:06 +0900

    画像処理追加

--- out/extension.js
+++ out/extension.js
@@ -19,41 +19,39 @@ class LocalStorage {
 
 function activate(context) {
 
-    //現在の曜日を取得
- let d = new Date();
- let day = d.getDate();  //日
- let dayofweek = d.getDay(); //曜日
- let changeDay = d.toLocaleDateString();
- let dayStr = d.toLocaleDateString();
+    
+        //現在の曜日を取得
+    let d = new Date();
+    let day = d.getDate();  //日
+    let dayofweek = d.getDay(); //曜日
+    let changeDay = d.toLocaleDateString();
+    let dayStr = d.toLocaleDateString();
 
  //-----------------------------------------------------------------------
 
 
-if(beforeDay == null){
-   storage.setValue("beforeDay", dayStr);
-   //現在の日付と前回の日付が違うか
-}else if(beforeDay != dayStr){
-    //テキストファイルが変更された回数を初期化
-    total_keypress_count = storage.setValue("total_keypress_count", 0);
-}
-
+// if(beforeDay == null){
+//    storage.setValue("beforeDay", dayStr);
+//    //現在の日付と前回の日付が違うか
+// }else if(beforeDay != dayStr){
+//     //テキストファイルが変更された回数を初期化
+//     total_keypress_count = storage.setValue("total_keypress_count", 0);
+// }
 
 
 
     //要るか分からん
     const storage = new LocalStorage(context.globalState);
 
-    storage.setValue("beforeDay", null);
+    let beforeDay;
 
-    
-
-    if(storage.getValue("beforeDay",) == null){
-        storage.setValue("beforeDay", dayStr);
-        //現在の日付と前回の日付が違うか
-     }
+    // if(storage.getValue("beforeDay",0) == null){
+    //     storage.setValue("beforeDay", dayStr);
+    //     //現在の日付と前回の日付が違うか
+    //  }
      //前回テキストファイルが変更された回数を取得
-    let beforeDay = storage.getValue("beforeDay", 5);
      
+     //beforeDay = storage.getValue("beforeDay", 0);
 
     let totalCouont = 0;
     
@@ -63,16 +61,32 @@ if(beforeDay == null){
     // //↓ちょっとこれ分からんかった
     label.show();
     context.subscriptions.push(label);
+    beforeDay = storage.getValue("beforeDay", 0);
+
     
     const updateLabel = () => {
         // format total_keypress_count as  1,234,567
         //let formatted_count = total_keypress_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         label.text = "$(flame)" + totalCouont + " " + beforeDay;
     };
+    
     updateLabel();
+    
 
     //テキストファイルが変更された回数を更新
     const onKeyPressed = () => {
+            //現在の曜日を取得
+        d = new Date();
+        day = d.getDate();  //日
+        dayofweek = d.getDay(); //曜日
+        changeDay = d.toLocaleDateString();
+        dayStr = d.toLocaleDateString();
+
+        //前回の日付の更新、変数のリセット
+        if(storage.getValue("beforeDay", 0) !== dayStr){
+            storage.setValue("beforeDay", dayStr);
+            beforeDay = storage.getValue("beforeDay", 0);
+        }
         //total_keypress_count = storage.getValue("total_keypress_count", 0);
         totalCouont++;
         //storage.setValue("total_keypress_count", total_keypress_count);
--- out/index.js
+++ out/index.js
@@ -1,6 +1,4 @@
 
-
-
 const content = document.getElementById('content');
 
 //曜日によって変更する位置が変更される
@@ -16,6 +14,14 @@ const dayArr = ['日', '月', '火', '水', '木', '金', '土'];
 
     let contentRow;
     let contentCol;
+
+    //左が濃い
+    let colorArr = [
+        ['#022D10', '#088D34', '#3AF478', '#CBFCDB'],//緑
+        ['#8F1209', '#EF1E0F', '#F34A3E', '#F9A49E'],//赤
+        ['#0E0157', '#1E02BC', '#4727FD', '#9D8CFE'] //青
+    ];
+
 //レイアウトの作成(col)
 for (let i = 0; i < 25; i++) {
     contentRow = document.createElement('div');
@@ -27,18 +33,20 @@ for (let i = 0; i < 25; i++) {
         contentCol.id = i * 10 + j;
         contentCol.setAttribute('class', 'contentCol');
         contentCol.style.width = '100px';
-        contentCol.style.height = '100px';
+        contentCol.style.height = '75px';
         contentCol.style.marginRight = '5px';
         contentCol.style.marginBottom = '5px';
         contentRow.appendChild(contentCol);
     }
 }
 
+
+let contentDay;
 //現在の日にちから現在の週の日曜日までの日付を出力
 for(let m = dayofweek; m >= 0; m--){
-    let contentDay = document.getElementById(m);
+    contentDay = document.getElementById(m);
     contentDay.innerHTML = dayStr;
-    contentDay.style.backgroundColor = '#f5f5f5';
+    contentDay.style.backgroundColor = '#022D10';
     day = d.setDate(d.getDate() - 1);
     dayStr = d.toLocaleDateString().substring(5);
 }
@@ -46,14 +54,102 @@ for(let m = dayofweek; m >= 0; m--){
 //残りの日付を出力
 for (let k = 1; k < 25; k++) {
     for (let l = 6; l >= 0; l--) {
-        let contentDay = document.getElementById(k * 10 + l);
+        contentDay = document.getElementById(k * 10 + l);
         contentDay.innerHTML = dayStr;
-        contentDay.style.backgroundColor = '#f5f5f5';
+        contentDay.style.backgroundColor = '#dcdcdc';
         day = d.setDate(d.getDate() - 1);
         dayStr = d.toLocaleDateString().substring(5);
         
     }
 }
+
+
+
+
+//【仮】試験的に、書いた回数、左が最新
+let totalCountArr = [4000,3000,2000,1000,0,2000,500];
+
+
+//【仮】選択した色の二次元配列の番号を入れる。緑=0、赤=1、青=2、初期値=0
+let selectColorNum = 0;
+//【仮】対象のdiv要素を示す変数
+let kariokiba;
+let i = 0;
+
+//初回起動時の色の振り分け
+for(let n = dayofweek; n >= 0; n--){
+    kariokiba = document.getElementById(n);
+    if(totalCountArr[i] >= 4000){
+        kariokiba.style.backgroundColor = colorArr[selectColorNum][0];
+    }else if(totalCountArr[i] >= 3000){
+        kariokiba.style.backgroundColor = colorArr[selectColorNum][1];
+    }else if(totalCountArr[i] >= 2000){
+        kariokiba.style.backgroundColor = colorArr[selectColorNum][2];
+    }else if(totalCountArr[i] >= 1000){
+        kariokiba.style.backgroundColor = colorArr[selectColorNum][3];
+    }else {
+        kariokiba.style.backgroundColor = '#dcdcdc';
+    }
+    i++;
+}
+for (let k = 1; k < 25; k++) {
+    for (let l = 6; l >= 0; l--) {
+        kariokiba = document.getElementById(k * 10 + l);
+        if(totalCountArr[i] >= 4000){
+            kariokiba.style.backgroundColor = colorArr[selectColorNum][0];
+        }else if(totalCountArr[i] >= 3000){
+            kariokiba.style.backgroundColor = colorArr[selectColorNum][1];
+        }else if(totalCountArr[i] >= 2000){
+            kariokiba.style.backgroundColor = colorArr[selectColorNum][2];
+        }else if(totalCountArr[i] >= 1000){
+            kariokiba.style.backgroundColor = colorArr[selectColorNum][3];
+        }else {
+            kariokiba.style.backgroundColor = '#dcdcdc';
+        }
+        i++;
+    }
+}
+
+document.getElementById('color').onchange = function(){
+    colorClick(this.value);
+}
+
+function colorClick(selectColorNum){
+    i = 0;
+    for(let n = dayofweek; n >= 0; n--){
+        kariokiba = document.getElementById(n);
+        if(totalCountArr[i] >= 4000){
+            kariokiba.style.backgroundColor = colorArr[selectColorNum][0];
+        }else if(totalCountArr[i] >= 3000){
+            kariokiba.style.backgroundColor = colorArr[selectColorNum][1];
+        }else if(totalCountArr[i] >= 2000){
+            kariokiba.style.backgroundColor = colorArr[selectColorNum][2];
+        }else if(totalCountArr[i] >= 1000){
+            kariokiba.style.backgroundColor = colorArr[selectColorNum][3];
+        }else {
+            kariokiba.style.backgroundColor = '#dcdcdc';
+        }
+        i++;
+    }
+    for (let k = 1; k < 25; k++) {
+        for (let l = 6; l >= 0; l--) {
+            kariokiba = document.getElementById(k * 10 + l);
+            if(totalCountArr[i] >= 4000){
+                kariokiba.style.backgroundColor = colorArr[selectColorNum][0];
+            }else if(totalCountArr[i] >= 3000){
+                kariokiba.style.backgroundColor = colorArr[selectColorNum][1];
+            }else if(totalCountArr[i] >= 2000){
+                kariokiba.style.backgroundColor = colorArr[selectColorNum][2];
+            }else if(totalCountArr[i] >= 1000){
+                kariokiba.style.backgroundColor = colorArr[selectColorNum][3];
+            }else {
+                kariokiba.style.backgroundColor = '#dcdcdc';
+            }
+            i++;
+        }
+    }
+}
+
 
 
 
